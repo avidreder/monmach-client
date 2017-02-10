ef50a79a1deb5b48ed36d4ec0fac93b61562fbfa
Andrew Reder <avidreder@gmail.com>
2017-02-09 19:13:44 -0800

    added some auth error handling

--- server/auth.js
+++ server/auth.js
@@ -1,35 +1,37 @@
-var request = require('request')
 const project = require('../config/project.config')
+const axios = require('axios')
 
 module.exports = {
   checkAuth: function(req, res, next) {
-    contactServer(req).then(function(result){
-      data = JSON.parse(result)
-      console.log(req.url)
-      console.log(req.headers.cookie)
-      console.log(data)
-      if (data.LoggedIn == false) {
-        res.status(401).send(`<h1>Login</h1><button><a href="${project.browser_api_path}/auth/spotify">Spotify</a></button>`)
-      } else {
-        next()
-      }
-    })
+    const loginPage = `<h1>Login</h1><button><a href="${project.browser_api_path}/auth/spotify">Spotify</a></button>`
+    contactServer(req)
+      .then(function(result){
+        if (result.LoggedIn == false) {
+          res.status(401).send(loginPage)
+        } else {
+          next()
+        }
+      })
+      .catch(function(error){
+        res.status(401).send(loginPage)
+      })
   }
 }
 
 function contactServer(req) {
   return new Promise(function(resolve, reject) {
     var options = {
-      url: `${project.server_api_path}/getuser`,
       headers: {
-        'Cookie': req.headers.cookie
+        'Cookie': req.headers.cookie,
+        'Content-Type': 'application/json',
       }
-    };
-    request(options, function(error, response, body) {
-      if (error) return reject(error)
-      resolve(body)
+    }
+    axios.get(`${project.server_api_path}/getuser`, options)
+      .then(function(body){
+        resolve(body.data)
+      })
+      .catch(function(error){
+        reject(error.response.data)
+      })
     })
-  }).then(function(body){
-    return body
-  })
 }
