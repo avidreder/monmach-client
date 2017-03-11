56113374981d79c3bd090f9da92bfa377b40b0f3
Andrew Reder <avidreder@gmail.com>
2017-03-11 08:58:03 -0800

    started refactoring queue items

--- src/components/QueueItem/QueueItem.jsx
+++ src/components/QueueItem/QueueItem.jsx
@@ -2,26 +2,33 @@ import React from 'react'
 import FontIcon from 'material-ui/FontIcon'
 import Paper from 'material-ui/Paper'
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar'
+import { Card, CardTitle, CardText, CardHeader } from 'material-ui/Card'
 
 export const QueueItem = (props) => (
-  <div>
-    <Paper>
-      <Toolbar>
-        <ToolbarGroup>
-          <ToolbarTitle text={props.track.SpotifyTrack.name} />
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <FontIcon className='material-icons'
-            onClick={() => props.setTrack(props.track)}>
-            play_circle_outline
-          </FontIcon>
-          <FontIcon className='material-icons'
-            onClick={() => props.discardTrackFromQueue(props.track)}>
-            not_interested</FontIcon>
-        </ToolbarGroup>
-      </Toolbar>
-    </Paper>
-  </div>
+
+    <Card>
+      <CardTitle>
+        <Toolbar>
+          <ToolbarGroup>
+            <ToolbarTitle text={props.track.SpotifyTrack.name} />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <FontIcon className='material-icons'
+              onClick={() => props.setTrack(props.track)}>
+              play_circle_outline
+            </FontIcon>
+            <FontIcon className='material-icons'
+              onClick={() => props.discardTrackFromQueue(props.track)}>
+              not_interested</FontIcon>
+          </ToolbarGroup>
+        </Toolbar>
+        { props.track.SpotifyTrack.artists.map((artist) => (
+          <Card key={ artist.id }>
+            <CardTitle subtitle={ artist.name } />
+          </Card>
+        ))}
+      </CardTitle>
+    </Card>
 )
 
 QueueItem.propTypes = {
