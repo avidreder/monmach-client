6b81c533ee2e83bbe2b50e9592f38bfcaa08a79c
Andrew Reder <avidreder@gmail.com>
2017-03-11 11:44:01 -0800

    added buttons to queue items

--- src/components/QueueItem/QueueItem.jsx
+++ src/components/QueueItem/QueueItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FontIcon from 'material-ui/FontIcon'
+import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar'
 import { Card, CardTitle, CardText, CardHeader, CardActions } from 'material-ui/Card'
@@ -13,13 +14,24 @@ export const QueueItem = (props) => {
     paddingBottom: '0px',
     paddingLeft: '16px'
   }
-  const queueActions = (<div style={ {float:'right'} }><FontIcon className='material-icons'
-    onClick={() => props.setTrack(props.track)}>
-    play_circle_outline
-  </FontIcon>
-  <FontIcon className='material-icons'
-    onClick={() => props.discardTrackFromQueue(props.track)}>
-    not_interested</FontIcon></div>)
+  const iconStyles = {
+    paddingTop: '0px',
+    paddingRight: '16px',
+    paddingBottom: '0px',
+    paddingLeft: '16px'
+  }
+  const queueActions = (<div style={ {float:'right'} }>
+    <IconButton onClick={() => props.setTrack(props.track)}>
+      <FontIcon className='material-icons'>
+        play_circle_outline
+      </FontIcon>
+    </IconButton>
+    <IconButton onClick={() => props.discardTrackFromQueue(props.track)} >
+      <FontIcon className='material-icons'>
+        not_interested
+      </FontIcon>
+    </IconButton>
+  </div>)
   return (
     <Card>
       <CardHeader title={ props.track.SpotifyTrack.name }
